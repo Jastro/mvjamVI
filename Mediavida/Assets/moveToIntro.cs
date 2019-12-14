8e24ecc707ff1ea4f4a8b648213efb36a6c76b17
Jacobo Caraballo <jacobo.caraballo@gmail.com>
2019-12-14 22:41:28 +0100

    added intro, and fonts

---
+++ Mediavida/Assets/moveToIntro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveToIntro : MonoBehaviour {
+    float currCountdownValue = 2;
+
+    public IEnumerator StartCountdown(float countdownValue = 2) {
+    currCountdownValue = countdownValue;
+    while(currCountdownValue > 0) {
+        yield return new WaitForSeconds(1.0f);
+        currCountdownValue--;
+        }
+    }
+
+    void Update() {
+        if(currCountdownValue == 0) {
+            SceneManager.LoadScene(2);
+        } else {
+            StartCoroutine(StartCountdown());
+        }
+    }
+}
