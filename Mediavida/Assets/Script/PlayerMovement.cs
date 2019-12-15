94d389fd71991d0307e42e358ec82eb906339222
Jacobo Caraballo <jacobo.caraballo@gmail.com>
2019-12-15 20:15:10 +0100

    fix game over, added enemys, added logic

--- Mediavida/Assets/Script/PlayerMovement.cs
+++ Mediavida/Assets/Script/PlayerMovement.cs
@@ -24,22 +24,30 @@ public class PlayerMovement : MonoBehaviour {
     void movement() {
         if(Input.GetKey(KeyCode.W)) {
             transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+            animator.SetBool("isMoving", true);
         }
 
         if(Input.GetKey(KeyCode.A)) {
             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            animator.SetBool("isMoving", true);
         }
 
         if(Input.GetKey(KeyCode.S)) {
             transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            animator.SetBool("isMoving", true);
         }
 
         if(Input.GetKey(KeyCode.D)) {
             transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            animator.SetBool("isMoving", true);
         }
 
         if (Input.GetMouseButtonDown(0) && canAttack) {
             attack();
+        }
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D)) {
+            animator.SetBool("isMoving", false);
         }
     }
 
--- Mediavida/Assets/Script/Scene/Intro/moveToInicio.cs
+++ Mediavida/Assets/Script/Scene/Intro/moveToInicio.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class moveToInicio : MonoBehaviour {
+    float currCountdownValue = 2;
+
+    public IEnumerator StartCountdown(float countdownValue = 2) {
+        currCountdownValue = countdownValue;
+        while(currCountdownValue > 0) {
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+    }
+
     void Update() {
-        if (Input.anyKey) {
+        if(currCountdownValue == 0) {
             SceneManager.LoadScene(0);
+        } else {
+            StartCoroutine(StartCountdown());
         }
     }
 }
